ac8418cfd79babec0cdbb1a84e066dcb4e71cd2f
Birkehoj <anders.birkehoj@gmail.com>
2016-01-31 21:25:14 +0100

    observation interface added

---
+++ activity_layer/src/observation_interface.h
@@ -0,0 +1,14 @@
+#ifndef OBSERVATION_INTERFACE_H
+#define OBSERVATION_INTERFACE_H
+
+
+class Observation_interface
+{
+public:
+    Observation_interface();
+    virtual void raytrace(int x0, int y0, int x1, int y1, bool markEnd) = 0;
+    virtual double getOccupancyPrabability(int x, int y) = 0;
+private:
+};
+
+#endif // OBSERVATION_INTERFACE_H
