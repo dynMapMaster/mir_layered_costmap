dc9e080e76ad78055b0dca4a793b34ef5aed4012
Birkehoj <anders.birkehoj@gmail.com>
2016-02-22 11:04:15 +0100

    mir run launch and map_msgs

---
+++ map_msgs/src/map_msgs/msg/__init__.py
@@ -0,0 +1,4 @@
+from ._ProjectedMap import *
+from ._OccupancyGridUpdate import *
+from ._ProjectedMapInfo import *
+from ._PointCloud2Update import *
---
+++ map_msgs/src/map_msgs/srv/__init__.py
@@ -0,0 +1,6 @@
+from ._SaveMap import *
+from ._ProjectedMapsInfo import *
+from ._GetPointMap import *
+from ._SetMapProjections import *
+from ._GetPointMapROI import *
+from ._GetMapROI import *
